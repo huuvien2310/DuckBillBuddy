88fe24e9556c3202e75f2fbc41e812ee898861ac
Vien Hua <huuvien2310@gmail.com>
2024-09-17 23:05:10 -0400

    add command to upload image

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import requests
 
 load_dotenv()
 
-TESTING_GUILD_ID = os.getenv('TESTING_GUILD_ID')
+TESTING_GUILD_ID = os.getenv("TESTING_GUILD_ID")
 
 intents = nextcord.Intents.all()
 intents.members = True
@@ -13,18 +13,29 @@ intents.message_content = True
 
 bot = commands.Bot(command_prefix="!", intents=intents)
 
+
 @bot.event
 async def on_ready():
-    print(f'We have logged in as {bot.user}')
+    print(f"We have logged in as {bot.user}")
+
 
 @bot.command()
 async def connection(ctx):
-    await ctx.send('Hello!')
+    await ctx.send("Hello!")
+
 
 @bot.command()
 async def augment(ctx):
-    response = requests.get("https://ddragon.leagueoflegends.com/cdn/13.24.1/data/en_US/tft-arena.json")
+    response = requests.get(
+        "https://ddragon.leagueoflegends.com/cdn/13.24.1/data/en_US/tft-arena.json"
+    )
     print(response.content)
     await ctx.send(response)
 
-bot.run(os.getenv('TOKEN'))
+
+@bot.command()
+async def image(ctx):
+    await ctx.send(file=nextcord.File('f0mpjw4ddzv41.png'))
+
+
+bot.run(os.getenv("TOKEN"))
